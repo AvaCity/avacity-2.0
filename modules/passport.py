1ec1d806fffe0c02c506f5bd2f700b1f2c08d3da
Ava.City <avacity@tuta.io>
2019-10-15 17:23:57 +0500

    modules/passport: Смотреть чужие паспорта

--- modules/passport.py
+++ modules/passport.py
@@ -8,7 +8,7 @@ class Passport(Module):
 
     def __init__(self, server):
         self.server = server
-        self.commands = {"sttrph": self.set_trophy}
+        self.commands = {"sttrph": self.set_trophy, "psp": self.passport}
 
     def set_trophy(self, msg, client):
         if msg[2]["trid"] not in tr:
@@ -18,3 +18,11 @@ class Passport(Module):
             trid = msg[2]["trid"]
             self.server.redis.set(f"uid:{client.uid}:trid", trid)
         client.send(["psp.sttrph", {"trid": trid}])
+
+    def passport(self, msg, client):
+        tr_ = {}
+        for item in tr:
+            tr_[item] = {"trrt": 0, "trcd": 0, "trid": item}
+        client.send(["psp.psp", {"psp": {"uid": msg[2]["uid"],
+                                         "ach": {"ac": {}, "tr": tr},
+                                         "rel": {}}}])
