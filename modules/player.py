9d9fdbbbaa3eddc14ad8f8326510c70af54c87ae
Ava.City <avacity@tuta.io>
2019-10-15 18:13:38 +0500

    modules/player: Кнопка "найти" в топе и списке знакомых

--- modules/player.py
+++ modules/player.py
@@ -8,7 +8,7 @@ class Player(Module):
 
     def __init__(self, server):
         self.server = server
-        self.commands = {"gid": self.players_by_id}
+        self.commands = {"gid": self.players_by_id, "flw": self.follow}
 
     def players_by_id(self, msg, client):
         players = []
@@ -18,3 +18,18 @@ class Player(Module):
                 continue
             players.append(plr)
         client.send(["pl.get", {"plrs": players, "clid": msg[2]["clid"]}])
+
+    def follow(self, msg, client):
+        user = None
+        for tmp in self.server.online.copy():
+            if tmp.uid == msg[2]["uid"]:
+                user = tmp
+                break
+        if not user:
+            scs = "userOffline"
+            locinfo = None
+        else:
+            scs = "success"
+            locinfo = {"st": 0, "s": "127.0.0.1", "at": None, "d": 0, "x": -1.0,
+                       "y": -1.0, "shlc": True, "pl": "", "l": tmp.room}
+        client.send(["pl.flw", {"scs": scs, "locinfo": locinfo}])
