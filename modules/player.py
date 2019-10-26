8e1d6ba304edf1b88030ffdf4466ca3665a9dfda
Ava.City <avacity@tuta.io>
2019-10-26 17:36:43 +0500

    modules/player: Должен быть user, а не tmp

--- modules/player.py
+++ modules/player.py
@@ -34,7 +34,7 @@ class Player(Module):
             scs = "success"
             locinfo = {"st": 0, "s": "127.0.0.1", "at": None, "d": 0,
                        "x": -1.0, "y": -1.0, "shlc": True, "pl": "",
-                       "l": tmp.room}
+                       "l": user.room}
         client.send(["pl.flw", {"scs": scs, "locinfo": locinfo}])
 
     def get_online_statuses(self, msg, client):
