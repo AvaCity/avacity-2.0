a30d57dcb2c6e68d0acb95ce5d8e8d459b13e894
Ava.City <avacity@tuta.io>
2019-10-26 21:35:45 +0500

    update: Перевод меню модератора сбоку

--- update.py
+++ update.py
@@ -140,6 +140,8 @@ async def process_config(version):
                 tasks.append(loop.create_task(download_furniture(url,
                                                                  session)))
             await asyncio.wait(tasks)
+    shutil.copyfile("files/avacity_ru.xml",
+                    "config_all_ru/translation/avacity_ru.xml")
     z = zipfile.ZipFile("files/data/config_all_ru.zip", mode="w")
     for root, dirs, files in os.walk(directory):
         for file in files:
